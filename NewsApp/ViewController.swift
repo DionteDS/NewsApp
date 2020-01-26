4086d00dc7c91ea08e3f4da2002b819facd42cf5
Dionte <Dionte_sb@yahoo.com>
2020-01-25 21:38:23 -0800

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Dionte Silmon on 1/25/20.
+//  Copyright © 2020 Dionte Silmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
