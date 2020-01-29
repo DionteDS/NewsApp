67c467fa6089b42ab1b3febd1b071e4fa5879b12
Dionte <Dionte_sb@yahoo.com>
2020-01-29 13:46:22 -0800

    setting up collectionViewCell for enterainment news

--- NewsApp/Categories/Controllers/Enterainment Controllers/EnterainmentViewController.swift
+++ NewsApp/Categories/Controllers/Enterainment Controllers/EnterainmentViewController.swift
@@ -14,20 +14,33 @@ class EnterainmentViewController: UIViewController {
 
     @IBOutlet weak var enterainmentCollectionView: UICollectionView!
     
-    private var flowLayout = UICollectionViewFlowLayout()
+    private var layout = UICollectionViewFlowLayout()
+    
+    private var enterainmentNews: [[String: Any]] = [[String: Any]]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print("In Enterainment View Controller")
+        
+        let nib = UINib(nibName: "EnterainmentCollectionViewCell", bundle: nil)
+        enterainmentCollectionView.register(nib, forCellWithReuseIdentifier: "enterainmentCell")
+        
+        setupLayout()
         
     }
     
     private func setupLayout() {
         
+        let collectionViewSizeWidth = enterainmentCollectionView.frame.size.width
+        let collectionViewSizeHeight = enterainmentCollectionView.frame.size.height
         
+        layout.itemSize = CGSize(width: collectionViewSizeWidth - 92, height: collectionViewSizeHeight - 518)
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+        layout.scrollDirection = .vertical
         
+        enterainmentCollectionView.setCollectionViewLayout(layout, animated: true)
     }
     
 
@@ -41,12 +54,15 @@ extension EnterainmentViewController: UICollectionViewDelegate, UICollectionView
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 6
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "enterainmentCell", for: indexPath) as! EnterainmentCollectionViewCell
+        
+        cell.titleLabel.text = "Title"
+        cell.sourceLabel.text = "Source"
         
         
         return cell
